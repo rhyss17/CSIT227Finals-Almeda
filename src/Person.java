d9280f2cbc8716d5e4ade2cd5756003bac3e8320
Rhyss <rhyssgianne123@gmail.com>
2022-12-21 19:59:18 +0800

    Disable text area for list of persons

--- src/Person.java
+++ src/Person.java
@@ -1,20 +1,19 @@
-public class Person {
-public abstract class person {
-    // TODO implement Person and its subclasses in other Java files
+public abstract class Person {
+    // TODO implement Person and its subclasses in  other Java files
     String name;
     int age;
 
-    public Person(String name, int age){
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    public String toString(){
-        return "Hello, my name is "+name;
+    public String toString() {
+        return "Hello, my name is " + name;
     }
 
     public static class Customer extends Person {
-        public Customer(String name, int age){
+        public Customer(String name, int age) {
             super(name, age);
         }
     }
@@ -22,7 +21,8 @@ public abstract class person {
     public static class Employee extends Person {
         int hours;
         int salary;
-        public Employee(String name, int age, int hours, int salary){
+
+        public Employee(String name, int age, int hours, int salary) {
             super(name, age);
             this.hours = hours;
             this.salary = salary;
@@ -30,14 +30,14 @@ public abstract class person {
     }
 
     public static class Clerk extends Employee {
-        public Clerk(String name, int age, int hours, int salary){
-            super(name,age,hours,salary);
+        public Clerk(String name, int age, int hours, int salary) {
+            super(name, age, hours, salary);
         }
     }
 
     public static class Manager extends Employee {
-        public Manager(String name, int age, int hours, int salary){
-            super(name,age,hours,salary);
+        public Manager(String name, int age, int hours, int salary) {
+            super(name, age, hours, salary);
         }
     }
 }
